ffc56c6b67599c55932181fef8b04b13ade85eef
Godfrey Chiu <ggwc82@gmail.com>
2016-03-17 09:39:21 +0000

    Cleared repo for new setup

--- config/initializers/session_store.rb
+++
@@ -1,3 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-Rails.application.config.session_store :cookie_store, key: '_makers_bnb_angular_rails_session'
--- protractor.conf.js
+++
@@ -1,9 +0,0 @@
-exports.config = {
-  framework: 'jasmine',
-  
-  seleniumAddress: 'http://localhost:4444/wd/hub',
-
-  baseUrl: 'http://localhost:3000/',
-
-  specs: ['test/e2e/testspec.js']
-}
