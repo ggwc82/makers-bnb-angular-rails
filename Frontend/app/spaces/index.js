e5ed610784027f7be4714adddb6a1762a0c3b2f2
Yan-Yi Li <liyanyi@gmail.com>
2016-03-17 11:12:37 +0000

    set up spaces controller

---
+++ Frontend/app/spaces/index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+angular.module('makersBnB.index', ['ngRoute'])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/spaces/index', {
+    templateUrl: 'spaces/index.html',
+    controller: 'SpacesCtrl'
+  });
+}])
+
+.controller('SpacesCtrl', [function() {
+
+}]);
