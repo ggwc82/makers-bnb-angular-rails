d2aecdf378ba260659de81fc1e78811d2812524c
Rebecca Piper <piper.rebecca93@gmail.com>
2016-03-18 10:12:29 +0000

    WIP Creating User Model

--- MakersBnB/config/routes.rb
+++ MakersBnB/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   match '*any' => 'application#options', :via => [:options]
 
   resources :spaces
@@ -8,7 +9,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'spaces#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
