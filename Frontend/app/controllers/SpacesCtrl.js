c4e43a0e1ad8b5ec05f3e329c543bb274e274e8b
David Parry <drjparry@gmail.com>
2016-03-21 10:57:26 +0100

    refactored service

--- Frontend/app/controllers/SpacesCtrl.js
+++ Frontend/app/controllers/SpacesCtrl.js
@@ -1,6 +1,6 @@
 'use strict';
 
-makersBnB.controller('SpacesCtrl', ['$http', '$location', function( $http, $location) {
+makersBnB.controller('SpacesCtrl', ['$http', '$location', 'SpacesService', function( $http, $location, SpacesService) {
   var self = this;
   self.spaces = [];
   self.currentspace;
@@ -12,11 +12,6 @@ makersBnB.controller('SpacesCtrl', ['$http', '$location', function( $http, $loca
     });
   };
 
-  self.getSpaces = function(){
-    $http.get('http://localhost:3000/api/spaces.json').then(function(response){
-      self.spaces = response.data.reverse();
-    });
-  };
 
   self.showSpace = function(space){
     $http.get('http://localhost:3000/api/spaces/' + space.id).then(function(response){
@@ -42,6 +37,77 @@ makersBnB.controller('SpacesCtrl', ['$http', '$location', function( $http, $loca
   };
 }]);
 
-.service('spaces', function(){
 
-})
+
+// 'use strict';
+
+// makersBnB.controller('SpacesCtrl', ['$http', '$location', 'spacesService', function( $http, $location, spacesService) {
+//   var self = this;
+
+//   self.create = function(space) {
+//     spacesService.createSpace(space);
+//   };
+
+//   self.getSpaces = function(){
+//     $http.get('http://localhost:3000/api/spaces.json').then(function(response){
+//       self.spaces = response.data.reverse();
+//     });
+//   };
+
+//   self.showSpace = function(space){
+//     spacesService.showSpace(space);
+//   };
+
+//   self.editSpace = function(space){
+//     $location.path('/spaces/edit/' + space.id)
+//   };
+
+//   self.updateSpace = function(space){
+//     spacesService.updateSpace(space);
+//   };
+
+//   self.deleteSpace = function(space){
+//     spacesService.deleteSpace(space);
+//   };
+
+// }]);
+
+// makersBnB.service('spacesService', ['$http', '$location', function($http, $location){
+//   var self = this;
+//   self.spaces = [];
+//   self.currentspace;
+
+//   this.createSpace = function(space){
+//     $http.post('http://localhost:3000/api/spaces', space).success(function(data) {
+//       self.spaces.push(data);
+//       self.showSpace(data.space);
+//     });
+//   }
+
+//   this.showSpace = function(space){
+//     $http.get('http://localhost:3000/api/spaces/' + space.id).then(function(response){
+//       self.currentspace = response.data;
+//       $location.path('/spaces/' + space.id)
+//     });
+//   };
+
+//   this.updateSpace = function(space){
+//     $http.put('http://localhost:3000/api/spaces/' + space.id, space).success(function(response){
+//       self.showSpace(response.space);
+//     });
+//   };
+
+//   self.deleteSpace = function(space){
+//     $http.delete('http://localhost:3000/api/spaces/' + space.id).then(function(data){
+//       $location.path('/')
+//     });
+//   };
+
+// }])
+
+
+
+
+
+
+
